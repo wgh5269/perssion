6c1a70fac4094a8e14fbf4258c813a429a8fca1a
wgh5269 <2105955290@qq.com>
2018-05-09 19:42:41 +0800

    新加权限点接口

---
+++ src/main/java/com/wgh/controller/SysAclController.java
@@ -0,0 +1,47 @@
+package com.wgh.controller;
+
+import com.google.common.collect.Maps;
+import com.wgh.beans.PageQuery;
+import com.wgh.common.JsonData;
+import com.wgh.model.SysRole;
+import com.wgh.param.AclParam;
+import com.wgh.service.SysAclService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/sys/acl")
+@Slf4j
+public class SysAclController {
+
+    @Resource
+    private SysAclService sysAclService;
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveAclModule(AclParam param) {
+        sysAclService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateAclModule(AclParam param) {
+        sysAclService.update(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/page.json")
+    @ResponseBody
+    public JsonData list(@RequestParam("aclModuleId") Integer aclModuleId, PageQuery pageQuery) {
+        return JsonData.success(sysAclService.getPageByAclModuleId(aclModuleId, pageQuery));
+    }
+
+}

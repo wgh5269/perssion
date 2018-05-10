f6cc348fa4f619b2f06a19f3fb569a6e8a8cf384
wgh5269 <2105955290@qq.com>
2018-05-10 15:18:28 +0800

    新增角色controller实现接口

---
+++ src/main/java/com/wgh/controller/SysRoleController.java
@@ -0,0 +1,51 @@
+package com.wgh.controller;
+
+
+
+import com.wgh.common.JsonData;
+import com.wgh.param.RoleParam;
+import com.wgh.service.SysRoleService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+
+
+/**
+ * Created by Administrator on 2018/5/10.
+ */
+@Controller
+@RequestMapping("/sys/role")
+public class SysRoleController {
+    @Resource
+    private SysRoleService sysRoleService;
+
+    @RequestMapping("role.page")
+    public ModelAndView page(){
+        return  new ModelAndView("role");
+    }
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveRole(RoleParam param){
+        sysRoleService.save(param);
+        return  JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public  JsonData  updateRole(RoleParam param){
+        sysRoleService.update(param);
+        return  JsonData.success();
+    }
+
+    @RequestMapping("/list.json")
+    @ResponseBody
+    public  JsonData  List(){
+        return JsonData.success(sysRoleService.getAll());
+    }
+
+}
